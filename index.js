bd8da06207543ed5dbeb6b11178493d00315d636
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-07-18 23:06:03 +0600

    commit

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-const client = new MongoClient(process.env.MONGODB_URI, {
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ymtlu.mongodb.net/`;
+const client = new MongoClient( uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
